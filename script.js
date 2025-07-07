7db1f7be93a5e773485f39a829d68f9a112d44aa
Jevi13 <jeanvicente2002@gmail.com>
2025-07-06 21:37:10 -0300

    Projeto finalizado, implementada as funcoes de composicao e evento de botao!

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 
 document.body.addEventListener('keyup', (event)=>{
-    playKey(event.code.toLowerCase);
+    playKey(event.code.toLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', (event)=>{
+    let song = document.querySelector('#input').value;
+    let songArray = song.split("");
+    song = document.querySelector('#input').value = "";
+    console.log(songArray);
+    playComp(songArray);
+}
+)
 
 function playKey(sound){
    
     let audioElement = document.querySelector(`#s_${sound}`);
-    console.log(audioElement);
+    let keyElement = document.querySelector(`.key[data-key="${sound}"]`); 
+     
+    if(audioElement){
+        audioElement.play();
+        audioElement.currentTime = 0;
+    }
 
+    if(keyElement){
+        keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        }, 300);
+
+    }
 }
 
+function playComp(songArray){
+
+    let wait = 0;
+    
+
+    for(let songitem of songArray){
+        setTimeout(()=>{
+            playKey(`key${songitem}`);
+        }, wait);
+        wait += 250;
+}
+
+}
